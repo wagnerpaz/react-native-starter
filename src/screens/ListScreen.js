38084a873f8592e404bf573dac204e4ae7e16bd1
Wagner Paz <wagner.o.paz@gmail.com>
2019-11-01 12:55:57 -0300

    Exercise 2: Add an age to the friend list created on the FlatList showcase part.

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        { name: 'Friend #1'},
-        { name: 'Friend #2'},
-        { name: 'Friend #3'},
-        { name: 'Friend #4'},
-        { name: 'Friend #5'},
-        { name: 'Friend #6'},
-        { name: 'Friend #7'},
-        { name: 'Friend #8'},
-        { name: 'Friend #9'},
+        { name: 'Friend #1', age: 21},
+        { name: 'Friend #2', age: 22},
+        { name: 'Friend #3', age: 23},
+        { name: 'Friend #4', age: 24},
+        { name: 'Friend #5', age: 25},
+        { name: 'Friend #6', age: 26},
+        { name: 'Friend #7', age: 27},
+        { name: 'Friend #8', age: 28},
+        { name: 'Friend #9', age: 29},
     ];
 
     return (
@@ -20,7 +20,7 @@ const ListScreen = () => {
             data={friends}
             renderItem={ ({item}) => {
             //element === { item: { name: 'Friend #1' }, index: 0 }
-            return <Text style={styles.textStyle}>{item.name}</Text>;
+            return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>;
         }}/>
     );
 };
