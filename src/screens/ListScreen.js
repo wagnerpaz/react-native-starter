009552b937671e8ffd1d3b6b32af81e5f699d0ad
Wagner Paz <wagner.o.paz@gmail.com>
2019-11-01 12:49:23 -0300

    Showcase: List of elements with FlatList.

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+const ListScreen = () => {
+    const friends = [
+        { name: 'Friend #1'},
+        { name: 'Friend #2'},
+        { name: 'Friend #3'},
+        { name: 'Friend #4'},
+        { name: 'Friend #5'},
+        { name: 'Friend #6'},
+        { name: 'Friend #7'},
+        { name: 'Friend #8'},
+        { name: 'Friend #9'},
+    ];
+
+    return (
+        <FlatList
+            keyExtractor={ friend => friend.name }
+            data={friends}
+            renderItem={ ({item}) => {
+            //element === { item: { name: 'Friend #1' }, index: 0 }
+            return <Text style={styles.textStyle}>{item.name}</Text>;
+        }}/>
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
+
+export default ListScreen;
