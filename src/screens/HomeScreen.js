4501d7f88ea36e34bcdd5c66f2a9865b424ce69e
Wagner Paz <wagner.o.paz@gmail.com>
2019-11-04 12:32:23 -0300

    Handled navigation on HomeScreen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => {
+          console.log('Button pressed');
+          navigation.navigate('Components');
+        }}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={ () => {
+          navigation.navigate('List');
+        }}
+      />
+
+      {/* DEMONSTRATE THE USE OF TouchableOpacity
+      <TouchableOpacity
+        onPress={() => {
+          console.log('List pressed')
+          navigation.navigate('List');
+        }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
